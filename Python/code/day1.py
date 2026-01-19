a147e1022aa9d59ce0f21265ac3f1ce7e6a8f733
coole <rosaghasemiansafai@gmail.com>
2026-01-20 00:30:29 +0100

    feature: add python day 1 code

---
+++ Python/code/day1.py
@@ -0,0 +1,12 @@
+# This is a single line comment, it starts with a hash #
+
+"""
+Use multi-line comments for longer explanations
+Multi-line comments start with triple quotes
+
+Why adding comments is important:
+- to help others (sometimes even your future self too) understand what your code does
+- provide context for any complex logic
+"""
+
+print("Hello, World!")
