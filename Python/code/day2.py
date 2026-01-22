aad21c28f63c4c1afa760a0438a06416b7ce3ee4
Coole <rosaghasemiansafai@gmail.com>
2026-01-22 01:45:52 +0100

    docs: add python day 2 answers

---
+++ Python/code/day2.py
@@ -0,0 +1,16 @@
+name = "CooleRearWindowSalad"
+age = 17
+height = 15.5
+is_student = True
+email = None
+siblings = [ "John", "anna" ]
+address = { "Street": "this street", "city": "Amsterdam" }
+
+print(f""" 
+Name: {name} 
+Age: {age} 
+Siblings: {siblings} 
+Is student: {is_student} 
+Email: {email} 
+Address: {address}
+""")
